5b96648f251d7ada5065feb992a8ade2c374b3d6
Soh1121 <satou.shg@gmail.com>
2022-02-01 08:43:20 +0900

    [add]JavaScriptのthisとは何なのかを学習
    thisを理解するため

---
+++ src/02-javascript/08-this/call-this.js
@@ -0,0 +1,8 @@
+const Person = function (name) {
+	this.name = name;
+
+	return this;
+};
+
+const kanae = Person.call({ gender: 'f' }, 'Kanae');	// { gender: 'f' } を this として渡す
+console.log(kanae);		// { gender: 'f', name: 'Kanae' }
